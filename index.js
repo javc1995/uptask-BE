d4be698d85b3fc5b31cdabb28d7f0e0adf25b13b
javc1995 <146193554+javc1995@users.noreply.github.com>
2023-09-28 17:12:44 -0400

    proyect ready

--- index.js
+++ index.js
@@ -38,7 +38,45 @@ const PORT = process.env.PORT || 4000
 
 console.log("desde index.js")
 
-app.listen(PORT, () => {
+const servidor = app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`)
 })
 
+// Socket IO
+import { Server } from 'socket.io'
+
+const io = new Server(servidor, {
+    pingTimeout: 60000,
+    cors : {
+        origin: process.env.FRONTEND_URL,
+    }
+})
+
+io.on('connection', (socket) => {
+    console.log('Conectado a socket.io');
+    // Definir los eventos de socket io
+    socket.on('abrir proyecto', (proyecto) => {
+        socket.join(proyecto)
+    })
+
+    socket.on('nueva tarea', (tarea) => {
+        const proyecto = tarea.proyecto;
+        io.to(proyecto).emit('tarea agregada', tarea);
+    });
+
+    socket.on('eliminar tarea', tarea => {
+        const proyecto = tarea.proyecto
+        io.to(proyecto).emit('tarea eliminada', tarea)
+    })
+
+    socket.on('actualizar tarea', tarea => {
+        const proyecto = tarea.proyecto._id
+        io.to(proyecto).emit('tarea actualizada',tarea)
+    })
+
+    socket.on('cambiar estado', tarea => {
+        const proyecto = tarea.proyecto._id
+        io.to(proyecto).emit('nuevo estado', tarea)
+    } )
+    
+})
